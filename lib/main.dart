b82abdc3392e08411e6d4b9e591dcaf2ce1d8b5e
Ahmed Nabil <ahmednab93@gmail.com>
2025-01-27 13:37:51 +0200

    feat: update the app entry point to the AnnounceKitWidget
    The change modifies the main.dart file and sets the `AnnounceKitWidget` as the new home screen.

--- lib/main.dart
+++ lib/main.dart
@@ -18,44 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(title: title),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              widget.title,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.of(context)
-            .push(MaterialPageRoute(builder: (_) => AnnounceKitWidget())),
-        tooltip: 'Go',
-        child: const Icon(Icons.arrow_forward),
-      ),
+      home: AnnounceKitWidget(),
     );
   }
 }
